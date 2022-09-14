8bac9513fc6a9c8e7fd6e7f4a0565d5677f0ac99
Никита Тристень <nick.tristen22@gmail.com>
2022-09-14 12:35:52 +0300

    update обновил сеттеры пользователей

--- src/store/users/mutations.js
+++ src/store/users/mutations.js
@@ -1,3 +1,21 @@
+export const SET_USERS_DATA = (state, data) => {
+    state.usersData = data
+    console.log('usersData', data)
+}
 export const SET_USERS = (state, data) => {
-    state.users = data
+    if (state.users?.length) {
+        state.users = state.users.concat(data)
+    } else {
+        state.users = data
+    }
+}
+
+export const SET_ROUTE = (state, data) => {
+    console.log('SET_ROUTE', data, state.route)
+    state.route = data
+}
+
+export const SET_LOADING_STATUS = (state, data) => {
+    console.log('SET_LOADING_STATUS', data, state.loading)
+    state.loading = data
 }
