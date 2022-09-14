8e0d95a4ca8b4c03bb1c40488eab224cb8cf4443
Никита Тристень <nick.tristen22@gmail.com>
2022-09-14 12:33:03 +0300

    new добавил наименование полей в соотсветствующий объект

--- src/apiOptions/index.js
+++ src/apiOptions/index.js
@@ -6,10 +6,37 @@ const routesNames = {
     TOKEN: 'token'
 }
 
+export const fieldNames = {
+    USERS_DATA: {
+        COUNT: 'count',
+        LINKS: 'links',
+        NEXT_URL: 'next_url',
+        PREV_URL: 'prev_url',
+        PAGE: 'page',
+        SUCCESS: 'success',
+        TOTAL_PAGES: 'total_pages',
+        TOTAL_USERS: 'total_users',
+        USERS: 'users'
+    },
+    USER: {
+        EMAIL: 'email',
+        ID: 'id',
+        NAME: 'name',
+        PHONE: 'phone',
+        PHOTO: 'photo',
+        POSITION: 'position',
+        POSITION_ID: 'position_id',
+        REGISTRATION_TIMESTAMP: 'registration_timestamp'
+
+    },
+    POSITION: {
+        NAME: 'name'
+    }
+}
 export const host = 'https://frontend-test-assignment-api.abz.agency/api/v1'
 
 export const routes = {
-    USERS: getRoute(routesNames.USERS),
+    USERS: `${getRoute(routesNames.USERS)}?page=1&count=6`,
     POSITIONS: getRoute(routesNames.POSITIONS),
     TOKEN: getRoute(routesNames.TOKEN)
 }
@@ -25,5 +52,10 @@ export async function apiRequest (method, url, data) {
         }
     }
 
-    return await axios({ method, url, data, config })
+    return await axios({
+        method,
+        url,
+        data,
+        config
+    })
 }
