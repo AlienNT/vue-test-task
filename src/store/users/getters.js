7297fe3437ae01e494df283e27fddd9bf5203809
Никита Тристень <nick.tristen22@gmail.com>
2022-09-14 12:32:08 +0300

    new добавил геттеры получения пользователя по id, роута следующей страницы и статуса загрузки пользователей

--- src/store/users/getters.js
+++ src/store/users/getters.js
@@ -1,3 +1,15 @@
 export const GET_USERS = (state) => {
     return state.users
 }
+export const GET_USER_BY_ID = (state) => (id) => {
+    return state.users.find(user => user.id === id)
+}
+export const GET_ROUTE = (state) => {
+    return state.route
+}
+export const GET_USERS_FIELD_BY_NAME = (state) => (fieldName) => {
+    return state.usersData[fieldName]
+}
+export const GET_LOADING_STATUS = (state) => {
+    return state.loading
+}
