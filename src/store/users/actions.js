d91efc4d34eb47793772d938090a158f2b4a6621
Никита Тристень <nick.tristen22@gmail.com>
2022-09-14 12:34:10 +0300

    update обновил функцию получения пользователей, добавил сохранение статуса загрузки

--- src/store/users/actions.js
+++ src/store/users/actions.js
@@ -1,15 +1,21 @@
-import { apiRequest, routes } from '@/apiOptions'
+import { apiRequest, fieldNames } from '@/apiOptions'
 
-export async function fetchUsers ({ commit }, page = 1, count = 6) {
-    return await apiRequest('GET', `${routes.USERS}?page=${page}&count=${count}`)
+export async function fetchUsersData ({ commit }, route) {
+    if (!route) return
+    commit('SET_LOADING_STATUS', true)
+    return await apiRequest('GET', route)
         .then(response => {
             if (response?.data?.success) {
+                commit('SET_LOADING_STATUS', false)
+                commit('SET_USERS_DATA', response?.data)
                 commit('SET_USERS', response?.data?.users)
+                commit('SET_ROUTE', response?.data[fieldNames.USERS_DATA.LINKS][fieldNames.USERS_DATA.NEXT_URL])
             }
             return response?.data
         })
         .catch(err => {
+            commit('SET_LOADING_STATUS', false)
             console.log('err', err)
-            return 'fetchUsers error'
+            return 'fetchUsersData error'
         })
 }
