cf61f6664399baf7b4c6fc11c3de1ea7b87ac928
Никита Тристень <nick.tristen22@gmail.com>
2022-09-14 12:39:54 +0300

    update обновил верстку и стили хэдера, добавил событие прокрутки к якорям

--- src/components/VHeader.vue
+++ src/components/VHeader.vue
@@ -1,30 +1,33 @@
 <template>
-  <header class="v-header">
-    <div class="v-container">
-      <div class="v-row header-row">
-        <div class="v-col">
-          <VLogo
-            path="/"
-            :src="logoImgPath"
-            alt="logo"
-          />
+    <header class="v-header">
+        <div class="v-container">
+            <div class="header-row">
+                <div class="logo-wrapper">
+                    <VLogo
+                        path="/"
+                        :src="logoImgPath"
+                        alt="logo"
+                    />
+                </div>
+                <div class="buttons-wrapper">
+                    <VButton
+                        label="Users"
+                        @onClick="scrollToComponent('userScreen')"
+                    />
+                    <VButton
+                        label="Sign up"
+                        @onClick="scrollToComponent('signupScreen')"
+                    />
+                </div>
+            </div>
         </div>
-        <div class="v-col buttons-wrapper">
-          <VButton
-            label="Users"
-          />
-          <VButton
-            label="Sign up"
-          />
-        </div>
-      </div>
-    </div>
-  </header>
+    </header>
 </template>
 
 <script>
 import VLogo from '@/components/VLogo'
 import VButton from '@/components/formComponents/VButton'
+
 export default {
     name: 'VHeader',
     components: {
@@ -35,22 +38,33 @@ export default {
         return {
             logoImgPath: require('../assets/images/Logo.svg')
         }
+    },
+    methods: {
+        scrollToComponent (refName) {
+            this.$store.dispatch('scrolling/setScrollEvent', refName)
+        }
     }
 }
 </script>
 
 <style scoped lang="scss">
 .v-header {
-  padding-top: 17px;
-  padding-bottom: 17px;
-  background: $headerBgColor;
+    display: flex;
+    align-items: center;
+    background: $headerBgColor;
+    height: 100%;
 }
+
 .header-row {
-  justify-content: space-between;
-  align-items: center;
+    justify-content: space-between;
+    align-items: center;
+    display: flex;
 }
+
 .buttons-wrapper {
-  display: flex;
-  gap: 10px;
+    display: flex;
+    flex: 1 1 auto;
+    justify-content: flex-end;
+    gap: 10px;
 }
 </style>
